debc8b27b55a0ecf09891886f47c3730d09247c0
laughterhao <laughterhao@gmail.com>
2023-07-23 18:52:14 +0800

    插件製作成卡片

--- src/components/application.vue
+++ src/components/application.vue
@@ -7,24 +7,79 @@
     </div>
     <div class="row text-center" style="padding-top: 30px">
       <div class="col">
-        <h3>DBI 應用</h3>
+        <div class="card" style="height: 564px;">
+          <img src="https://fakeimg.pl/300x200/200">
+          <div class="card-body">
+            <h5 class="card-title">DBI 應用</h5>
+            <p class="card-text">
+              目前來看，大氣層的USB直裝遊戲未來應該是趨向於Tinfoil和DBI方式了，也省去了關機拔記憶卡動作。主要介紹DBI的USB遊戲安裝，卸載補丁，重置遊戲所需最低系統版本等。
+            </p>
+            <a href="#" class="btn btn-primary">閱讀更多...</a>
+          </div>
+        </div>
       </div>
       <div class="col">
-        <h3>JKSV</h3>
+        <div class="card" style="height: 564px;" >
+          <img src="https://fakeimg.pl/300x200/200">
+          <div class="card-body">
+            <h5 class="card-title">JKSV</h5>
+            <p class="card-text">
+              常用存檔導入匯出功能的人應該知道兩款插件程式：JKSV和CheckPoint。
+              JKSV除了存檔導入、匯出、批量備份、功能以外，更常用的一個功能是BCAT的恢復，常玩動物之森和怪物獵人崛起的人可能會用到當然還有一些比較少用到的功能，比如刪除本機已下載的系統升級包。
+            </p>
+            <a href="#" class="btn btn-primary">閱讀更多...</a>
+          </div>
+        </div>
       </div>
       <div class="col">
-        <h3>DayBreak</h3>
+        <div class="card" style="height: 564px;">
+          <img src="https://fakeimg.pl/300x200/200">
+          <div class="card-body">
+            <h5 class="card-title">DayBreak</h5>
+            <p class="card-text">
+              離線升級系統，對於破解設備來說是很有必要的，畢竟隨著遊戲更新，系統版本的要求也會隨之遞增。鑒於大白兔很久沒有更新了，而大氣層官方推薦使用Daybreak來離線升級主機系統。
+              所以，今天就來說一下大氣層 Atmosphere 如何離線升降級系統。
+            </p>
+            <a href="#" class="btn btn-primary">閱讀更多...</a>
+          </div>
+        </div>
       </div>
     </div>
     <div class="row text-center" style="padding-top: 30px">
       <div class="col">
-        <h3>Linkalho</h3>
+        <div class="card" style="height: 564px;">
+          <img src="https://fakeimg.pl/300x200/200">
+          <div class="card-body">
+            <h5 class="card-title">Linkalho</h5>
+            <p class="card-text">
+              有部分遊戲，如暗黑3，真人快打、重裝機兵等，啟動遊戲均需要本地帳戶有綁定任天堂的帳戶才行，但是破解系統又不能正常連線去任天堂做帳戶的綁定(會被ban)所以國外大神rdmrocha開發了一款名為Linkalho的工具，可以實現離線模式下關聯全部帳戶，一鍵關聯全部帳戶，一鍵解除關聯全部帳戶，支援當前所有Switch設備 SX軟破、硬破設備和大氣層設備。
+            </p>
+            <a href="#" class="btn btn-primary">閱讀更多...</a>
+          </div>
+        </div>
       </div>
       <div class="col">
-        <h3>大氣層包升級</h3>
+        <div class="card" style="height: 564px;">
+          <img src="https://fakeimg.pl/300x200/200">
+          <div class="card-body">
+            <h5 class="card-title">大氣層包升級</h5>
+            <p class="card-text">
+              由於任天堂,會不斷的更新韌體版本,為了要讓大氣層可以支援最新的韌體,我們必須更新更新大氣層包
+            </p>
+            <a href="#" class="btn btn-primary">閱讀更多...</a>
+          </div>
+        </div>
       </div>
       <div class="col">
-        <h3>金手指使用</h3>
+        <div class="card" style="height: 564px;">
+          <img src="https://fakeimg.pl/300x200/200">
+          <div class="card-body">
+            <h5 class="card-title">Tesle側邊選單</h5>
+            <p class="card-text">Tesla側邊快捷選單為一個可快速開啟特定功能的選單可自訂義想加入的功能，可達到快速開啟及關閉的作用選單可以在系統啟動後於主頁面或是遊戲內按下 左搖桿的 L 鍵 + 方向下 鍵 來啟動
+            </p>
+            <a href="#" class="btn btn-primary">閱讀更多...</a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
